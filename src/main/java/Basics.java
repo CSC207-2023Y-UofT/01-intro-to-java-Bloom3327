3469ff53431ad608bc3d6cce0171e7b6636e3c37
H <habib.azim@mail.utoronto.ca>
2023-06-11 20:33:52 -0400

    Java exercise 1 for csc207.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -100,7 +100,7 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-       for (int j = 1; j <= 10; j++) {
+       for (int j = 10; j >= 0; j--) {
            System.out.println("Current count: " + j);
        }
     }
